d6906fc332fd35ba6995f156633b81e6a01cf075
Nikita Smagin <n.smagin@iqdev.digital>
2022-03-17 00:05:54 +0500

    Fix problems with modules

--- src/app.js
+++ src/app.js
@@ -4,4 +4,7 @@ const characters = [
   { name: 'маг', health: 0 },
   { name: 'лучник', health: 0 },
 ];
-characters.filter((item) => item.health > 0);
+
+const alive = characters.filter((item) => item.health > 0);
+
+console.log(alive);
